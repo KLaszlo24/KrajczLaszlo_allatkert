2f0f7546dc8b1e01aebd7f23415a72f5e442efc9
KLaszlo24 <krajczlaci234@gmail.com>
2025-02-28 11:36:23 +0100

    trópusi oszt. kesz

---
+++ OPPallatkert/Trópusi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPallatkert
+{
+	internal class Trópusi
+	{
+		int sebesseg;
+		string nev;
+		string nem;
+		int eletkor;
+
+		public Trópusi(int sebesseg, string nev, string nem, int eletkor)
+		{
+			this.sebesseg = sebesseg;
+			this.nev = nev;
+			this.nem = nem;
+			this.eletkor = eletkor;
+		}
+
+		public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+		public string Nev { get => nev; set => nev = value; }
+		public string Nem { get => nem; set => nem = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+	}
+}
